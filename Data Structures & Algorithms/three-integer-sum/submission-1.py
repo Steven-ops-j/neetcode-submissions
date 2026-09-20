326ae5a36a47b6d148bbde2d98b0b0212b7fa8c8
Steven Chen <60072131+Steven-ops-j@users.noreply.github.com>
2026-09-20 12:38:53 -0500

    Add: three-integer-sum - submission-1

---
+++ Data Structures & Algorithms/three-integer-sum/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        mp = {}
+        for i, value in enumerate(nums):
+            if value not in mp:
+                mp[value] = []
+            mp[value].append(i)
+        exists = set() 
+        output = [] 
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                need = -(nums[i] + nums[j])
+                if need in mp:
+                    for ind in mp[need]:
+                        x = tuple(sorted([nums[i], nums[j], nums[ind]]))
+                        if ind != i and ind != j:
+                            if x not in exists:
+                                exists.add(x)
+                                output.append([nums[i], nums[j], nums[ind]]) 
+                            break
+        return output
